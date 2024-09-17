7fd1ada74c588fd10db4de38b469dc1d8646f48e
Aisha-Fathima <149351295+Aisha-Fathima@users.noreply.github.com>
2024-09-17 22:23:34 +0530

    Update code.sql

--- DATABASE FILE/code.sql
+++ DATABASE FILE/code.sql
@@ -1,27 +1,14 @@
 -- phpMyAdmin SQL Dump
--- version 5.2.0
+
 -- https://www.phpmyadmin.net/
---
--- Host: localhost:3306
--- Generation Time: Feb 22, 2023 at 08:03 AM
--- Server version: 8.0.30
--- PHP Version: 8.1.10
+
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
 SET time_zone = "+00:00";
 
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
 
---
--- Database: `onlinefoodphp`
---
-
--- --------------------------------------------------------
 
 --
 -- Table structure for table `admin`
